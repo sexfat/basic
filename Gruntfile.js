d1407434bd370c3ebf37629155b2699ad980db04
sexfat <bryant6831@gmail.com>
2017-04-07 22:31:38 +0800

    update

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,21 +1,24 @@
 module.exports = function(grunt) {
 
-  // Project configuration.
+  //Project configuration.
   grunt.initConfig({
 
-    uglify: {
+  uglify: {
     my_target: {
       files: {
         'dest/output.js': ['src/*.js']
+             // dest目的 : src 來源
       }
     }
   }
   });
 
-  // Load the plugin that provides the "uglify" task.
+  //Load the plugin that provides the "uglify" task.
   grunt.loadNpmTasks('grunt-contrib-uglify');
 
   // Default task(s).
   grunt.registerTask('default', ['uglify']);
+  // grunt.registerTask('default', ['uglify']);
+
 
 };
